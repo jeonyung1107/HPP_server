054c3b010f3a958e6ba563b60a1f6947963ba78c
jeon <jeonyng@gmail.com>
2019-01-07 01:04:00 +0900

    .

--- src/main/java/CaffeineGorilla/HPP_server/Controllers/HomeController.java
+++ src/main/java/CaffeineGorilla/HPP_server/Controllers/HomeController.java
@@ -5,18 +5,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/index")
+@RequestMapping("/")
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<String> mainHandler(){
-        List<String> list = new ArrayList<String>();
+    public Map<String, String> mainHandler(){
+        Map<String, String> map = new HashMap<String, String>();
 
-        for (int i = 0; i < 10; ++i) list.add("test" + i);
+        for(int i=0; i<10; ++i){
+            map.put(Integer.toString(i),"value" + i);
+        }
 
-        return list;
+        return map;
     }
 }
