fa23f0bff80b1c299de0e625f739df5400ccd7ca
gorilla <jeonyng@gmail.com>
2019-04-14 17:19:02 +0900

    .

--- src/main/java/CaffeineGorilla/HPP_server/Configs/user_table.sql
+++ src/main/java/CaffeineGorilla/HPP_server/Configs/user_table.sql
@@ -1,3 +1,6 @@
+create user if not exists hpp idenetified by '1234'
+grant all privileges on hpp.* to 'hpp'@'localhost' identified by '1234';
+
 CREATE TABLE IF NOT EXISTS user(
 	id varchar(100) not null unique,
 	password char(60) not null,
