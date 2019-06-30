7d0c161baea3242d1105dc9903bd60a13ea6a4eb
jeon <jeonyng@gmail.com>
2019-06-30 17:27:42 +0900

    add data, make DDL use utf8

--- configs/HPP_schema.sql
+++ configs/HPP_schema.sql
@@ -4,14 +4,15 @@ CREATE TABLE IF NOT EXISTS user(
 	authority char(60) not null,
 	enabled boolean not null default true,
 	primary key (id)
-);
+)default character set utf8 collate utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS muscle(
 	id char(10) primary key ,
 	name varchar(100) not null ,
+	korean varchar(100),
 	parent char(10),
 	foreign key (parent) references muscle(id)
-);
+)default character set utf8 collate utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS workout(
 	id char(10) primary key ,
@@ -19,24 +20,24 @@ CREATE TABLE IF NOT EXISTS workout(
 	muscle char(10) not null ,
 	unit char(10),
 	foreign key (muscle) references muscle(id)
-);
+)default character set utf8 collate utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS session(
 	id char(16) primary key ,
 	start timestamp default current_timestamp,
 	end timestamp default current_timestamp on update current_timestamp
-);
+)default character set utf8 collate utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS workout_set(
     id        char(10) primary key,
     session   char(16) not null,
-    excercise char(10) not null,
+    workout char(10) not null,
     intensity int      not null,
     numberof  int      not null,
     writtentime      timestamp default current_timestamp,
-    foreign key (excercise) references workout (id),
+    foreign key (workout) references workout (id),
     foreign key (session) references session (id)
-);
+)default character set utf8 collate utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS user_session(
 	user varchar(100),
@@ -44,7 +45,7 @@ CREATE TABLE IF NOT EXISTS user_session(
 	primary key (user, session),
 	foreign key (user) references user(id),
 	foreign key (session) references session(id)
-)
+)default character set utf8 collate utf8_general_ci;
 
 
 
--- configs/data.sql
+++ configs/data.sql
@@ -1,14 +1,20 @@
-INSERT into muscle(id, name) VALUES ("DH6unzClxG", "chest");;
-INSERT into muscle(id, name, parent) VALUES ("yG8GOzVh3X", "pectoralis major","DH6unzClxG");
-INSERT into muscle(id, name, parent) VALUES ("dvrSQLopev", "pectoralis major upper side","yG8GOzVh3X");
-INSERT into muscle(id, name, parent) VALUES ("jzJamIhu2j", "pectoralis major under side","yG8GOzVh3X");
-INSERT into muscle(id, name) VALUES ("sEsEhhCNae", "arm");
-INSERT into muscle(id, name, parent) VALUES ("2E9h4I2u5o", "Biceps brachii", "sEsEhhCNae");
-INSERT into muscle(id, name, parent) VALUES ("0WGhUb6ba9", "Triceps", "sEsEhhCNae");
-INSERT into muscle(id, name, parent) VALUES ("IYFbcvkbTe", "antebrachial muscles", "sEsEhhCNae");
-INSERT into muscle(id, name) VALUES ("IYFbcvkbTe", "back");
+INSERT into muscle(id, name, korean) VALUES ("DH6unzClxG", "chest", "가슴");
+INSERT into muscle(id, name, korean, parent) VALUES ("yG8GOzVh3X", "pectoralis major","대흉근","DH6unzClxG");
+INSERT into muscle(id, name, korean, parent) VALUES ("dvrSQLopev", "pectoralis major upper side", "대흉근 상부","yG8GOzVh3X");
+INSERT into muscle(id, name, korean, parent) VALUES ("jzJamIhu2j", "pectoralis major under side", "대흉근 하부","yG8GOzVh3X");
+INSERT into muscle(id, name, korean) VALUES ("sEsEhhCNae", "arm", "팔");
+INSERT into muscle(id, name, korean, parent) VALUES ("2E9h4I2u5o", "Biceps brachii", "이두근", "sEsEhhCNae");
+INSERT into muscle(id, name, korean, parent) VALUES ("0WGhUb6ba9", "Triceps", "삼두근", "sEsEhhCNae");
+INSERT into muscle(id, name, korean, parent) VALUES ("IYFbcvkbTe", "antebrachial muscles", "전완근", "sEsEhhCNae");
+INSERT into muscle(id, name, korean) VALUES ("IYFbcvkbTe", "back", "등");
+INSERT into muscle(id, name, korean, parent) VALUES ("3fsT7KhkxM", "Latissimus dorsi", "광배근", "IYFbcvkbTe");
+INSERT into muscle(id, name, korean, parent) VALUES ("3QUPLL7dH6", "Trapezius Muscle", "승모근", "IYFbcvkbTe");
+INSERT into muscle(id, name, korean, parent) VALUES ("lFNsA6p1oQ", "spinal erector muscle", "척추기립근", "IYFbcvkbTe");
+INSERT into muscle(id, name, korean) VALUES ("IZzod35d3u", "leg", "다리");
+INSERT into muscle(id, name, korean, parent)
+VALUES ("WaCFI95Ms9", "Gluteus maximus", "대둔근", "IZzod35d3u"),("3lxSxNs3Pf", "femoral muscle", "대퇴근", "IZzod35d3u");
 
-INSERT INTO workout(id, name, muscle, unit) VALUES ("testtest01", "testtest01", "testmuscle", "KG");
-INSERT INTO workout(id, name, muscle, unit) VALUES ("testtest02", "testtest02", "testparent", "KG");
-INSERT INTO workout(id, name, muscle, unit) VALUES ("testtest03", "testtest03", "testchild1", "KG");
+INSERT INTO workout(id, name, muscle, unit) VALUES ("1x0W279amU", "biceps arm curl", "2E9h4I2u5o", "KG");
+INSERT INTO workout(id, name, muscle, unit) VALUES ("NCzanU2aN7", "triceps extension", "0WGhUb6ba9", "KG");
+INSERT INTO workout(id, name, muscle, unit) VALUES ("WG9XtAC7xB", "deadlift", "lFNsA6p1oQ", "KG");
 
