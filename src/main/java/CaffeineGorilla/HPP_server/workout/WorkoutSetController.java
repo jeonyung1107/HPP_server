40d0a0505a4bd5657815c75f238c1f38775318c2
jeon <jeonyng@gmail.com>
2019-07-19 21:03:23 +0900

    change log config

--- src/main/java/CaffeineGorilla/HPP_server/workout/WorkoutSetController.java
+++ src/main/java/CaffeineGorilla/HPP_server/workout/WorkoutSetController.java
@@ -1,16 +1,11 @@
 package CaffeineGorilla.HPP_server.workout;
 
-import CaffeineGorilla.HPP_server.model.Session;
-import CaffeineGorilla.HPP_server.model.UserSession;
-import CaffeineGorilla.HPP_server.model.WorkoutSet;
 import CaffeineGorilla.HPP_server.workout.service.SessionService;
 import CaffeineGorilla.HPP_server.workout.service.UserSessionService;
 import CaffeineGorilla.HPP_server.workout.service.WorkoutSetService;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,8 +30,6 @@ public class WorkoutSetController {
     public WorkoutRequest.WorkoutSetRequest writeWorkoutSet(WorkoutRequest.WorkoutSetRequest workoutSetRequest){
 
         workoutSetService.writeWorkoutSet(workoutSetRequest);
-
-        logger.info(workoutSetRequest.toString());
 
         return workoutSetRequest;
     }
